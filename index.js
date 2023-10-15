8f57b7d97c2a056662f0ec6e2fa53788a3d7e19b
Julian Gabriel <juliangabriel570@gmail.com>
2023-10-15 20:23:22 +0200

    init

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+const express = require('express');
 const { Rettiwt } = require('rettiwt-api');
+
+const app = express();
+const port = process.env.PORT || 3000; // You can change the port as needed
 
 const API_KEY = 'kdt=RNfNZsgXfO8gOOIk3TfOuKCI4JYEhfVgBzxPhL31;twid="u=1713605010930462720";ct0=10d89a41073544b186803dd7d50d8c0d;auth_token=a14c1066ba1ab2c93a9dd6ffd2989501c007cba2';
 
 // Creating a new Rettiwt instance using the API_KEY
 const rettiwt = new Rettiwt(API_KEY);
 
-// Fetching the details of the user whose username is <username>
-rettiwt.user.details('elonmusk')
-.then(details => {
-    console.log(details);
-})
-.catch(error => {
-    console.log(error);
+// Define a route to fetch user details by username
+app.get('/:username', (req, res) => {
+    const username = req.params.username;
+
+    // Fetching the details of the user whose username is provided in the URL
+    rettiwt.user.details(username)
+        .then(details => {
+            res.json(details);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message });
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
